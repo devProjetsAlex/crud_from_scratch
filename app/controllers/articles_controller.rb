f6d5dba9d36ce8070bb0c663e3986601caf1a84f
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-04-18 12:45:10 -0400

    created message add on

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,8 +15,8 @@ class ArticlesController < ApplicationController
         @article = Article.new(params.require(:article).permit(:title, :description))
        
        if @article.save
+        flash[:notice] = "Article was created successfully."
         redirect_to @article
-
        else
         render 'new'
     end
