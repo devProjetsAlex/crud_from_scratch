96dcb152e394737532e4894942e73145d5b20631
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-04-20 14:29:35 -0400

    user profil show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
   def new
     @user = User.new
   end
@@ -7,6 +11,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
 
   def update
     @user = User.find(params[:id])
