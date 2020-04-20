616d1e6a7a0238a0f3dd83735de03c2676a89df7
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-04-20 14:48:55 -0400

    user list and avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @articles = @user.articles
   end
+
+  def index
+    @users = User.all 
+  end
+
+
+
   def new
     @user = User.new
   end
